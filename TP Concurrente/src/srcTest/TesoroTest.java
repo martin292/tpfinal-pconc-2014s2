f0de05ae6083abfa27caf1f36634c5efc5e7f3e4
ingridgcalderon@gmail.com <ingridgcalderon@gmail.com@1f189421-3a66-92ff-6b36-92dd8fdf890e>
2014-11-24 20:07:06 +0000

    Agrego la clase TesoroTest

---
+++ TP Concurrente/src/srcTest/TesoroTest.java
@@ -0,0 +1,39 @@
+package srcTest;
+
+import static org.junit.Assert.*;
+import juego.Posicion;
+import juego.Tesoro;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesoroTest {
+	
+	private Tesoro tesoro;
+	private Posicion posicion;
+
+	@Before
+	public void setUp(){
+		posicion = new Posicion(2,3);
+		tesoro = new Tesoro(posicion);
+	}
+
+	@Test
+	public void testSetUp() {
+		assertEquals(tesoro.getPos(), posicion);
+		assertFalse(tesoro.fueConquistado());
+	}
+	
+	@Test
+	public void testSetPosicion() {
+		Posicion pos2 = new Posicion(4,2);
+		tesoro.setPos(pos2);
+		assertEquals(tesoro.getPos(), pos2);
+	}
+
+	@Test
+	public void testSetFueConquistado() {
+		tesoro.setConquistado(true);
+		assertTrue(tesoro.fueConquistado());
+	}
+}
