a5f28761f38284301d10f7f1e2e182ff0f4e87da
ingridgcalderon@gmail.com <ingridgcalderon@gmail.com@1f189421-3a66-92ff-6b36-92dd8fdf890e>
2014-11-24 19:58:11 +0000

    Update Posición

--- TP Concurrente/src/juego/Posicion.java
+++ TP Concurrente/src/juego/Posicion.java
@@ -4,20 +4,38 @@ public class Posicion {
 
 	private int x;
 	private int y;
+	private boolean tieneTesoro;
+	private boolean tieneExplorador;
+	private boolean estaEnBorde;
 	
+	/**
+	 * Crea una posición con las coordenadas x e y.
+	 * También se inicializa sin tesoros y sin exploradores
+	 * @param x
+	 * @param y
+	 */
 	public Posicion(int x, int y){
 		this.x = x;
 		this.y = y;
+		this.tieneTesoro = false;
+		this.tieneExplorador = false;
 	}
 	
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Posicion && 
+        		((Posicion)obj).x == this.x && 
+        		((Posicion)obj).y == this.y;
+    }
+    
 	/**
-	 * Retorna true si las coordenadas son iguales
-	 * @param x2
-	 * @param y2
-	 * @return
+	 * Devuelve si la posición está ocupada, es decir, si contiene un 
+	 * tesoro o un explorador
+	 * @return true si tiene un explorador o un tesoro, false si no tiene ni
+	 * tesoro ni explorador
 	 */
-	public boolean igual(int x2, int y2) {
-		return this.x == x2 && this.y == y2;
+	public boolean posicionOcupada(){
+		return this.tieneTesoro || this.tieneExplorador;
 	}
 	
 	//--------------------------------------------------------------------
@@ -25,15 +43,41 @@ public class Posicion {
 	public int getX() {
 		return x;
 	}
+	
 	public void setX(int x) {
 		this.x = x;
 	}
+	
 	public int getY() {
 		return y;
 	}
+	
 	public void setY(int y) {
 		this.y = y;
 	}
+	
+	public boolean estaEnBorde(){
+		return this.estaEnBorde;
+	}
+	
+	public void setEstaEnBorde(boolean estaEnBorde){
+		this.estaEnBorde = estaEnBorde;
+	}
+	
+	public boolean tieneTesoro(){
+		return this.tieneTesoro;
+	}
+	
+	public void setTieneTesoro(boolean tieneTesoro){
+		this.tieneTesoro = tieneTesoro;
+	}
+	
+	public boolean tieneExplorador(){
+		return this.tieneExplorador;
+	}
+	
+	public void setTieneExplorador(boolean tieneExplorador){
+		this.tieneExplorador = tieneExplorador;
 
-			
+	}
 }
