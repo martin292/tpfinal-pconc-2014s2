8c425358558c0a3c67fbfda4a022d83cec753209
ingridgcalderon@gmail.com <ingridgcalderon@gmail.com@1f189421-3a66-92ff-6b36-92dd8fdf890e>
2014-11-24 20:25:50 +0000

    Update Explorador

--- TP Concurrente/src/juego/Explorador.java
+++ TP Concurrente/src/juego/Explorador.java
@@ -10,45 +10,48 @@ public class Explorador extends Thread{
 		this.pos = p;
 		this.team = e;
 	}
-	
-	
+		
 	/**
-	 * Mientras que los tesoros no hayan sido conquistados, el explorador debe moverse aleatoriamente en una direccion.
+	 * Mientras que queden tesoros por conquistar, el explorador debe 
+	 * moverse aleatoriamente en una direccion.
 	 * DERECHA   -> 1
 	 * IZQUIERDA -> 2
 	 * NORTE     -> 3
 	 * SUR       -> 4
 	 */
 	public void run(){
-		while(!this.team.tesorosConquistados()){
-			
+		while(!this.team.todosMisTesorosConquistados()){
 			int num = 1 + (int)(Math.random()*4);
-			
 			try{
-			switch(num) {
-			 case 1: 
-				 this.moverEste();				
-			     break;
-			 case 2: 
-				 this.moverOeste();			
-			     break;
-			 case 3: 
-				 this.moverNorte();
-			     break;
-			 case 4: 
-				 this.moverSur();
-			     break;
-			}
-			}catch(InterruptedException e){e.printStackTrace();}
+				switch(num) {
+				  case 1: 
+					  this.moverEste();				
+					  break;
+				  case 2: 
+					  this.moverOeste();			
+					  break;
+				  case 3: 
+					  this.moverNorte();
+					  break;
+				  case 4: 
+					  this.moverSur();
+					  break;
+					}
+				} catch(InterruptedException e){
+						e.printStackTrace();
+						}
 		}
 	}	
 	
 	/**
-	 * Si la posicion existe mueve al explorador a la derecha
+	 * Si existe una posición a la derecha del explorador, éste se mueve
+	 * hacia esa direccion
 	 * @throws InterruptedException
 	 */
 	public void moverEste() throws InterruptedException{
-		if(this.tablero.existePos(this.getPos().getX()+1, this.getPos().getY())){
+		Posicion posAux = new Posicion (this.getPos().getX()+1, this.getPos().getY());
+		
+		if(this.tablero.existePos(posAux)){
 			this.tablero.moverDer(this);
 		 }
 		//Cuando un participante intenta un movimiento que lo llevaria fuera del tablero, se entera de que esta en el borde.
@@ -59,7 +62,9 @@ public class Explorador extends Thread{
 	 * @throws InterruptedException
 	 */
 	public void moverOeste() throws InterruptedException{
-		if(this.tablero.existePos(this.getPos().getX()-1, this.getPos().getY())){
+		Posicion posAux = new Posicion(this.getPos().getX()-1, this.getPos().getY());
+		
+		if(this.tablero.existePos(posAux)){
 			this.tablero.moverIzq(this);
 		 }
 		//Cuando un participante intenta un movimiento que lo llevaria fuera del tablero, se entera de que esta en el borde.
@@ -70,7 +75,9 @@ public class Explorador extends Thread{
 	 * @throws InterruptedException
 	 */
 	public void moverNorte() throws InterruptedException{
-		if(this.tablero.existePos(this.getPos().getX(), this.getPos().getY()+1)){
+		Posicion posAux = new Posicion(this.getPos().getX(), this.getPos().getY()+1);
+		
+		if(this.tablero.existePos(posAux)){
 			this.tablero.moverNorte(this);
 		 }
 		//Cuando un participante intenta un movimiento que lo llevaria fuera del tablero, se entera de que esta en el borde.
@@ -81,13 +88,14 @@ public class Explorador extends Thread{
 	 * @throws InterruptedException
 	 */
 	public void moverSur() throws InterruptedException{
-		if(this.tablero.existePos(this.getPos().getX(), this.getPos().getY()-1)){
+		Posicion posAux = new Posicion(this.getPos().getX(), this.getPos().getY()-1);
+		
+		if(this.tablero.existePos(posAux)){
 			this.tablero.moverSur(this); 
 		 }
-		//Cuando un participante intenta un movimiento que lo llevaria fuera del tablero, se entera de que esta en el borde.
+		//Cuando un participante intenta un movimiento que lo llevaria fuera del 
+		//tablero, se entera de que esta en el borde.
 	}
-	
-	
 	
 	//--------------------------------------------------------------------
 	
@@ -105,5 +113,4 @@ public class Explorador extends Thread{
 	public void setTablero(Tablero tablero) {
 		this.tablero = tablero;
 	}
-	
 }
